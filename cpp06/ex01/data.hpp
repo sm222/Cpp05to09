5d472deb76e795262012c8dcc5e6bb50cfc674a5
Antoine <soleil.maraska@gmail.com>
2024-01-27 18:14:09 -0500

    working on cpp 06/ex01

--- cpp06/ex01/data.hpp
+++ cpp06/ex01/data.hpp
@@ -1,2 +1,17 @@
-#ifdef DATA_
-# define DATA_
+
+#ifndef DATA_H
+# define DATA_H
+
+# include <iostream>
+# include <cstdint>
+
+struct Data
+{
+  int i;
+  char c;
+};
+
+uintptr_t serialize(Data *data);
+Data *deserialize(uintptr_t raw);
+
+#endif // DATA_H
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,6 +1,14 @@
 
-uinptr_t
+#include "data.hpp"
 
 int  main(void) {
+  Data A;
 
+  A.c = 'a';
+  A.i = 42;
+  uintptr_t ptr = serialize(&A);
+  std::cout << ptr << std::endl;
+  
+  Data  *B = deserialize(ptr);
+  std::cout << "c = " << B->c << " i = " << B->i << std::endl;
 }
