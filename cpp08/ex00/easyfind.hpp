83addf1c4807b9833c86b0019f7e31ef26e2988d
sm222 <soleil.maraska@gmail.com>
2024-02-12 11:28:02 -0500

    v1 of cpp 08/00 and start 08/02

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,16 @@
+
+#ifndef EASYFIND_HPP_
+# define EASYFIND_HPP_
+
+# include <iostream>
+
+template <typename T>
+typename T::iterator easyfind(T container, int i) {
+  typename T::iterator it = std::find(container.begin(), container.end(), i);
+  if (it == container.end())
+    throw std::invalid_argument("i is not in container");
+  return (it);
+}
+
+
+#endif // EASYFIND_HPP_
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,5 +1,35 @@
-
+# include "easyfind.hpp"
+# include <vector>
+# include <iostream>
 
 int main(void) {
+  std::vector<int>  list;
+  for (size_t i = 1; i < 10; i++) {
+    list.push_back(42 * i);
+  }
+  //test 1
+  try {
+    std::vector<int>::iterator it = easyfind(list, 42);
+    std::cout << *it << std::endl;
+  }
+  catch(const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  //test 2
+  try {
+    std::vector<int>::iterator it = easyfind(list, 168);
+    std::cout << *it << std::endl;
+  }
+  catch(const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  //test 3
+  try {
+    std::vector<int>::iterator it = easyfind(list, 160);
+    std::cout << *it << std::endl;
+  }
+  catch(const std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
   return (0);
 }
